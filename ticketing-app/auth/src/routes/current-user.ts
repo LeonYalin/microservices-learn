a53db96c630ef25df5de0f86489e76a1a9bcdc30
Leon Yalin <leon.yalin@irisity.com>
2024-05-16 16:40:50 +0300

    create reusable common npm package

--- ticketing-app/auth/src/routes/current-user.ts
+++ ticketing-app/auth/src/routes/current-user.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { currentUser } from '../middlewares/current-user';
+import { currentUser } from '@leonyalintickets/common';
 
 const router = express.Router();
 
